a7d930b83be6a03c0052d09debf11df027e55d99
cpurez8023 <1147944802@qq.com>
2017-11-23 10:48:10 +0800

    Initial commit

---
+++ GitDemoNoinit/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  GitDemoNoinit
+//
+//  Created by gotop on 2017/11/23.
+//  Copyright © 2017年 gotop. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ GitDemoNoinit/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  GitDemoNoinit
+//
+//  Created by gotop on 2017/11/23.
+//  Copyright © 2017年 gotop. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
